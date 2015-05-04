650566914c186b2d288f6858010a384c4ab1b55b
Hoe Kit CHEW <hoekit@gmail.com>
2015-05-04 16:20:52 +0700

    Add functions to the library

--- lib/WWW/Mechanize/Firefox/Extended.pm
+++ lib/WWW/Mechanize/Firefox/Extended.pm
@@ -3,10 +3,14 @@ package WWW::Mechanize::Firefox::Extended;
 use 5.006;
 use strict;
 use warnings FATAL => 'all';
+use parent 'WWW::Mechanize::Firefox';
+
+use Time::HiRes qw/usleep/;
+my $USLEEP_INTERVAL = 200000;   # 200 milliseconds
 
 =head1 NAME
 
-WWW::Mechanize::Firefox::Extended - The great new WWW::Mechanize::Firefox::Extended!
+WWW::Mechanize::Firefox::Extended - Adds handy functions to WWW::Mechanize::Firefox
 
 =head1 VERSION
 
@@ -19,35 +23,93 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+Module provides handy functions to check existence of selectors on a page and
+to wait for selectors to come into existence.
 
     use WWW::Mechanize::Firefox::Extended;
 
-    my $foo = WWW::Mechanize::Firefox::Extended->new();
-    ...
+    my $mech = WWW::Mechanize::Firefox::Extended->new();
+    $mech->get('https://www.example.com/');
 
-=head1 EXPORT
+    $mech->hasAll('#username', '#password', '#Image1');
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    $mech->hasAny('.close-button', '.exit-button', '.out-button');
+
+    $mech->waitAll(5, '#slow-loading-element')
+        or die "Expected element not found";
+
+    $mech->waitAny(5, '#slow-loading-element', '#another-element')
+        or die "Expected element not found";
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 hasAll( $mech, @selectors )
+
+Returns true if all selectors exists. False otherwise.
 
 =cut
-
-sub function1 {
+sub hasAll {
+    my $m = shift;
+    for (@_) {
+        return 0 if scalar ($m->selector($_,all=>1)) == 0;  # short-circuit
+    }
+    return 1;
 }
 
-=head2 function2
+=head2 hasAny( $mech, @selectors )
+
+Returns true if any selector exists. False if none exists.
 
 =cut
-
-sub function2 {
+sub hasAny {
+    my $m = shift;
+    for (@_) {
+        return 1 if scalar ($m->selector($_,all=>1)) > 0;  # short-circuit
+    }
+    return 0;
 }
+
+=head2 waitAll( $mech, $max_wait_seconds, @selectors )
+
+Wait until all selectors are present or the wait times out.
+Returns true if all selectors found or false if none found 
+within the timeout period.
+
+Uses Time::HiRes
+
+=cut
+sub waitAll {
+    my ($m, $timeout, @selectors) = @_;
+    my ($slept, $max_sleep) = (0, $timeout * 1000000); # microseconds
+    while ($slept < $max_sleep) {
+        return 1 if (hasAll($m, @selectors));
+        usleep($USLEEP_INTERVAL);    # sleep 200 milliseconds
+        $slept += $USLEEP_INTERVAL;
+    }
+    return 0;
+}
+
+=head2 waitAny( $mech, $max_wait_seconds, @selectors )
+
+Wait until any selectors are present or the wait times out.
+Returns true if any selectors are or false if none found 
+within the timeout period.
+
+Uses Time::HiRes
+
+=cut
+sub waitAny {
+    my ($m, $timeout, @selectors) = @_;
+    my ($slept, $max_sleep) = (0, $timeout * 1000000); # microseconds
+    while ($slept < $max_sleep) {
+        return 1 if (hasAny($m, @selectors));
+        usleep($USLEEP_INTERVAL);    # sleep 200 milliseconds
+        $slept += $USLEEP_INTERVAL;
+    }
+    return 0;
+}
+
+
 
 =head1 AUTHOR
 
@@ -58,9 +120,6 @@ Hoe-Kit Chew, C<< <hoekit at gmail dot com> >>
 Please report any bugs or feature requests to C<bug-www-mechanize-firefox-extended at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Mechanize-Firefox-Extended>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -91,50 +150,20 @@ L<http://search.cpan.org/dist/WWW-Mechanize-Firefox-Extended/>
 
 =back
 
+=head1 REPOSITORY AND PULL REQUESTS
 
-=head1 ACKNOWLEDGEMENTS
+This module is available on GitHub at
+L<https://github.com/hoekit/www-mechanize-firefox-extended/>. 
 
+Pull requests welcomed.
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2015 Hoe-Kit Chew.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.10.0 or, at
+your option, any later version of Perl 5 you may have available.
 
 =cut
 
